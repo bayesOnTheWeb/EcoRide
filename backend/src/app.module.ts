ef3a73fe4d3b49c0dedddc3e98e40f9922f86839
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-11-06 16:16:57 +0100

    SYNC all

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { RegisterModule } from './register/register.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
-
+    // TypeORM
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -13,12 +16,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      autoLoadEntities : true,
+      autoLoadEntities: true,
       synchronize: true,
-      retryAttempts : 10,
-      retryDelay : 3000
+      retryAttempts: 10,
+      retryDelay: 3000,
     }),
 
+    // Throttler global
+    ThrottlerModule.forRoot({
+      ttl: 60,   // période en secondes
+      limit: 10, // nombre max de requêtes par période
+    } as any),
+
+    // Modules fonctionnels
+    AuthModule,
+    RegisterModule,
   ],
   controllers: [AppController],
   providers: [AppService],
