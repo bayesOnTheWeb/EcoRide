6ed50774619b669bd8eaf3b9875d7e5305247eb4
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-11-06 12:13:16 +0100

    INIT TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities : true,
+      synchronize: true,
+      retryAttempts : 10,
+      retryDelay : 3000
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
