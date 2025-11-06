9b8175e7c7b1275b1d1d8709394474008c5f6b57
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-11-06 15:59:29 +0100

    ADD security core

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin:[process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
