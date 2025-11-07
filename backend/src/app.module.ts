942a196feb5b3a1ab78974b867563c58d7df842e
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-11-07 23:02:35 +0100

    ADD security core

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+
+    ThrottlerModule.forRoot()
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet()),
+  app.enableCors({
+    origin : ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }),
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,      
+      forbidNonWhitelisted: true, 
+      transform: true,       
+    }),
+  );
+
+
+  await app.listen(process.env.BACK_PORT ?? 3001);
 }
 bootstrap();
