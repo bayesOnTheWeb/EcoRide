b08ba3099154041dc3d1304868578bec6f6c2d8b
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-11-07 23:11:21 +0100

    ADD TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities : true, 
+      synchronize: true,
+    }),
 
     ThrottlerModule.forRoot()
 
